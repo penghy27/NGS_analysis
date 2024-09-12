a8d2b29f15d9eecef96d5a80748008f2939b2e01
penghy27 <penghsiaoyu27@gmail.com>
2024-09-12 17:09:06 -0400

    add files

---
+++ TranscriptomeAssembly/scripts/analyzeTrinityDeNovo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/evn bash
+# analyzeTrinityDeNovo.sh
+# Usage: bash scripts/analyzeTrinityDeNovo.sh 1>results/trinity_de_novo_stats.txt 2>results/logs/analyzeTrinityDeNovo.err
+
+TrinityStats.pl results/trinity_de_novo/Trinity.fasta
